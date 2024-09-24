713e60c8d7a389b14e5e68b501758498085b3029
CNGamer1 <cngamerz3@gmail.com>
2024-09-24 15:24:03 -0700

    Update BlockDetectingHandler.java

--- src/main/java/net/cngamer/burburaddons/BlockDetectingHandler.java
+++ src/main/java/net/cngamer/burburaddons/BlockDetectingHandler.java
@@ -1,21 +1,34 @@
 package net.cngamer.burburaddons;
 
-import net.minecraftforge.event.world.BlockEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.client.event.RenderWorldLastEvent;
-import net.minecraft.util.Vec3;
+import net.minecraft.client.Minecraft;
 import net.minecraft.init.Blocks;
-import net.minecraft.block.state.IBlockState;
-import net.cngamer.burburaddons.Utils;
-import java.awt.Color;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.Vec3;
+import net.minecraft.world.World;
+import net.minecraftforge.client.event.RenderWorldLastEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
+
+import java.awt.*;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Stack;
 
 public class BlockDetectingHandler {
+
     private static final int min_X = 0;
-    private static final int max_X = 100;
-    private static final int min_Y = 0;
-    private static final int max_Y = 10;
+    private static final int max_X = 2;
+    private static final int y_coord = 70;
     private static final int min_Z = 0;
-    private static final int max_Z = 100;
+    private static final int max_Z = 2;
+    private int deadbushCount = 0;
+    private boolean deletedLines = true;
+
+    private final Set hasDeadbush = new HashSet();
+    private final Stack storedLocations = new Stack();
+    private final Stack deadbushPositions = new Stack();
+
+    private boolean changeGreenLine = false;
     private Vec3 blockOne;
     private Vec3 blockTwo;
     private Vec3 blockThree;
@@ -24,46 +37,84 @@ public class BlockDetectingHandler {
     private Vec3 blockSix;
     private Vec3 blockSeven;
     private Vec3 blockEight;
-    private boolean changeGreenLine = false;
-
-    private boolean inField(int x, int y, int z) {
-        return x >= min_X && x <= max_X && y >= min_Y && y <= max_Y && z >= min_Z && z <= max_Z;
-    }
-
-    private boolean isDeadBush(IBlockState state) {
-        return state.getBlock() == Blocks.deadbush;
-    }
-
-    private boolean areVec3Equal(Vec3 vec1, Vec3 vec2) {
-        return vec1 != null && vec2 != null && vec1.xCoord == vec2.xCoord && vec1.yCoord == vec2.yCoord && vec1.zCoord == vec2.zCoord;
-    }
 
     @SubscribeEvent
-    public void onBlockNeighborNotify(BlockEvent.PlaceEvent event) {
-        if (inField(event.pos.getX(), event.pos.getY(), event.pos.getZ()) && isDeadBush(event.placedBlock)) {
-            Vec3 blockPos = new Vec3(event.pos.getX() + 0.5, event.pos.getY() + 0.5, event.pos.getZ() + 0.5);
-            if (blockEight == null) {
-                blockEight = blockPos;
-            } else if (blockSeven == null) {
-                blockSeven = blockPos;
-            } else if (blockSix == null) {
-                blockSix = blockPos;
-            } else if (blockFive == null) {
-                blockFive = blockPos;
-            } else if (blockFour == null) {
-                blockFour = blockPos;
-            } else if (blockThree == null) {
-                blockThree = blockPos;
-            } else if (blockTwo == null) {
-                blockTwo = blockPos;
+    public void onClientTick(TickEvent.ClientTickEvent event) {
+        World world = Minecraft.getMinecraft().theWorld;
+        if (world != null) {
+            if (deadbushPositions.size() < 8 && deletedLines) {
+                for (int x = min_X; x <= max_X; x++) {
+                    for (int z = min_Z; z <= max_Z; z++) {
+                        BlockPos BlockPosition = new BlockPos(x, y_coord, z);
+                        if (hasDeadbush.contains(BlockPosition)) {
+                            continue;
+                        }
+                        if (world.getBlockState(BlockPosition).getBlock() == Blocks.deadbush) {
+                            hasDeadbush.add(BlockPosition);
+                            storedLocations.push(BlockPosition);
+                            Vec3 BlockPos = new Vec3(x + 0.5, y_coord + 0.5, z + 0.5);
+                            deadbushPositions.push(BlockPos);
+                            deadbushCount++;
+                        }
+                    }
+                }
             } else {
-                blockOne = blockPos;
+                deletedLines = false;
+                if (deadbushCount == 8) {
+                    blockOne = (Vec3) deadbushPositions.get(7);
+                    blockTwo = (Vec3) deadbushPositions.get(6);
+                    blockThree = (Vec3) deadbushPositions.get(5);
+                    blockFour = (Vec3) deadbushPositions.get(4);
+                    blockFive = (Vec3) deadbushPositions.get(3);
+                    blockSix = (Vec3) deadbushPositions.get(2);
+                    blockSeven = (Vec3) deadbushPositions.get(1);
+                    blockEight = (Vec3) deadbushPositions.get(0);
+                }
+                if (deadbushCount > 0) {
+                    BlockPos BlockPositions = (BlockPos) storedLocations.get(deadbushCount - 1);
+                    if (world.getBlockState(BlockPositions).getBlock() == Blocks.air) {
+                        deadbushPositions.pop();
+                        storedLocations.pop();
+                        hasDeadbush.remove(BlockPositions);
+                        switch (deadbushCount) {
+                            case 1:
+                                blockEight = null;
+                                break;
+                            case 2:
+                                blockSeven = null;
+                                break;
+                            case 3:
+                                blockSix = null;
+                                break;
+                            case 4:
+                                blockFive = null;
+                                break;
+                            case 5:
+                                blockFour = null;
+                                break;
+                            case 6:
+                                blockThree = null;
+                                break;
+                            case 7:
+                                blockTwo = null;
+                                break;
+                            case 8:
+                                blockOne = null;
+                                changeGreenLine = true;
+                                break;
+                        }
+                        deadbushCount--;
+                    }
+                } else {
+                    deletedLines = true;
+                }
             }
         }
     }
 
     @SubscribeEvent
     public void onWorldRender(RenderWorldLastEvent event) {
+
         if (blockOne != null && blockTwo != null) {
             Utils.drawLine(blockOne, blockTwo, Color.GREEN, event.partialTicks);
         }
@@ -86,36 +137,5 @@ public class BlockDetectingHandler {
             Utils.drawLine(blockSeven, blockEight, changeGreenLine && blockSix == null ? Color.GREEN : Color.RED, event.partialTicks);
         }
     }
-
-    @SubscribeEvent
-    public void onBlockBreakEvent(BlockEvent.BreakEvent event) {
-        if (inField(event.pos.getX(), event.pos.getY(), event.pos.getZ()) && isDeadBush(event.state)) {
-            Vec3 blockBroken = new Vec3(event.pos.getX() + 0.5, event.pos.getY() + 0.5, event.pos.getZ() + 0.5);
-            if (areVec3Equal(blockBroken, blockOne)) {
-                blockOne = null;
-                changeGreenLine = true;
-            }
-            if (areVec3Equal(blockBroken, blockTwo)) {
-                blockTwo = null;
-            }
-            if (areVec3Equal(blockBroken, blockThree)) {
-                blockThree = null;
-            }
-            if (areVec3Equal(blockBroken, blockFour)) {
-                blockFour = null;
-            }
-            if (areVec3Equal(blockBroken, blockFive)) {
-                blockFive = null;
-            }
-            if (areVec3Equal(blockBroken, blockSix)) {
-                blockSix = null;
-            }
-            if (areVec3Equal(blockBroken, blockSeven)) {
-                blockSeven = null;
-            }
-            if (areVec3Equal(blockBroken, blockEight)) {
-                blockEight = null;
-            }
-        }
-    }
 }
+
